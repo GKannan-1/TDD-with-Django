a5500857e574bdf049bb17c5c41ac7f74c4a32fe
Gautham Kannan <130305877+ZDDwidow@users.noreply.github.com>
2025-03-15 15:37:23 -0400

    First unittest and view function

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,13 @@
 from django.test import TestCase
+from django.http import HttpRequest, HttpResponse
+from .views import home_page
 
 
-class SmokeTest(TestCase):
-    def test_bad_maths(self):
-        self.assertEqual(1+1, 3)
+class HomePageTest(TestCase):
+    def test_home_page_correct_html(self):
+        request = HttpRequest()
+        response: HttpResponse = home_page(request)
+        html: str = response.content.decode("utf8")
+        self.assertIn("<title>To-Do lists<title>", html)
+        self.assertTrue(html.startswith("<html>"))
+        self.assertTrue(html.endswith("<html>"))
