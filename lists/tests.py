970aea6dc3f2cd2584578cf5498505ea7d9448a1
Gautham Kannan <130305877+ZDDwidow@users.noreply.github.com>
2025-03-19 12:35:39 -0400

    new URL, refactored tests

--- lists/tests.py
+++ lists/tests.py
@@ -9,22 +9,6 @@ class HomePageTest(TestCase):
         response: HttpResponse = self.client.get("/")
         self.assertTemplateUsed(response, "home.html")
 
-    def test_can_save_a_POST_request(self):
-        self.client.post("/", data={"item_text": "A new list item"})
-        self.assertEqual(Item.objects.count(), 1)
-        # Using index instead of first() as first() can return None if the Item.objects is empty, but we are already checking if it's empty
-        new_item: Item = Item.objects.all()[0]
-        self.assertEqual(new_item.text, "A new list item")
-
-    def test_redirects_after_POST(self):
-        response: HttpResponse = self.client.post(
-            "/", data={"item_text": "A new list item"})
-        self.assertRedirects(response, "/lists/the-only-list-in-the-world/")
-
-    def test_only_saves_items_when_necessary(self):
-        self.client.get("/")
-        self.assertEqual(Item.objects.count(), 0)
-
 
 class ListViewTest(TestCase):
     def test_uses_list_template(self):
@@ -38,6 +22,20 @@ class ListViewTest(TestCase):
             "/lists/the-only-list-in-the-world/")
         self.assertContains(response, "itemey 1")
         self.assertContains(response, "itemey 2")
+
+
+class NewListTest(TestCase):
+    def test_can_save_a_POST_request(self):
+        self.client.post("/lists/new", data={"item_text": "A new list item"})
+        self.assertEqual(Item.objects.count(), 1)
+        # Using index instead of first() as first() can return None if the Item.objects is empty, but we are already checking if it's empty
+        new_item: Item = Item.objects.all()[0]
+        self.assertEqual(new_item.text, "A new list item")
+
+    def test_redirects_after_POST(self):
+        response: HttpResponse = self.client.post(
+            "/lists/new", data={"item_text": "A new list item"})
+        self.assertRedirects(response, "/lists/the-only-list-in-the-world/")
 
 
 class ItemModelTest(TestCase):
